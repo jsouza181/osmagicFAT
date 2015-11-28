4482f3e10984a2333f8633b661cc108c28f52f5d
Ricardo Castilla <castillaricardo19@gmail.com>
2015-11-28 00:08:01 -0500

    added logic to add equal spacing when executing ls

--- commands.c
+++ commands.c
@@ -6,17 +6,27 @@
 
 // Find the names of each file/directory in the given directory and print.
 void ls(Directory dir) {
-  // Short name maximum of 13 characters (11 for name, 1 for '.', 1 for \0)
-  char entryName[13];
+  // Short name maximum of MAX_ENTRY characters (11 for name, 1 for '.', 1 for \0)
+  int MAX_ENTRY = 13;
+  int i = 0;
+  int spaceInt = 0;
+  char spacing[MAX_ENTRY];
+  char entryName[MAX_ENTRY];
 
-  for(int i = 0; i < dir.size; ++i) {
+  for(i = 0; i < dir.size; ++i) {
     getShortName(dir.dirEntries[i], entryName);
 
     if(i % 4 == 0 && i != 0) // newline spacing
       printf("\n%s ", entryName);
-    else
+    else {
       printf("%s ", entryName);
-    //printf("size: %d", (int)strlen(entryName));
+    }
+    // printf("size: %d", (int)strlen(entryName));
+    for (spaceInt = 0; spaceInt < MAX_ENTRY - (int)strlen(entryName); ++spaceInt) {
+      spacing[spaceInt] = ' ';
+    } // for
+    spacing[spaceInt] = '\0';
+    printf("%s", spacing);
   }
   printf("\n");
 }
